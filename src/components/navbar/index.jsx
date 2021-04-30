008908afce555ccdfa2bb1bead7a055259166c37
SwarzChen <aaaa102234@gmail.com>
2021-04-30 19:16:53 +0800

    [main] create navbar container

---
+++ src/components/navbar/index.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import "./index.css"
+
+const NavBar = () => {
+    return (    
+        <div className="navbar-container">
+            <img src="image/Logo.svg" />
+        </div>
+    );
+}
+
+export default NavBar;
