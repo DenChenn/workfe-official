6af2cfd79e7d0daf3b282bd6332d5716401d3dd1
SwarzChen <aaaa102234@gmail.com>
2021-04-30 19:16:21 +0800

    [main] finish pin animation

---
+++ src/components/map/pin.jsx
@@ -0,0 +1,37 @@
+import React from "react"
+import {motion} from "framer-motion"
+import "./pin.css"
+
+const Pin = ({isOpen}) => {
+    const PinVariant = {
+        "open": {
+            transform: "translateY(0%)",
+            opacity: 1,
+        },
+        "closed": {
+            transform: "translateY(-100%)",
+            opacity: 0,
+        }
+    }
+
+    const PinTransition = {
+        duration: 1,
+        delay: 2
+    }
+
+    return (
+        <div className="pin-container">
+            <motion.div 
+                className="pin"
+                animate={isOpen ? "open" : "closed"}
+                initial={false}
+                variants={PinVariant}
+                transition={PinTransition}
+            >
+                <img className="pin-svg" src="image/pin.svg" alt=""/>
+            </motion.div>
+        </div>
+    );
+}
+
+export default Pin;
