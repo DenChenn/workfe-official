ca0c15501989f641e98495e3f010139cbc224118
SwarzChen <aaaa102234@gmail.com>
2021-04-30 19:15:51 +0800

    [main] finish phone component

---
+++ src/components/map/phone.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import "./phone.css"
+
+const Phone = () => {
+    return (
+        <div className="phone-container">
+            <img className="phone" src="image/phone.svg" />
+        </div>
+    );
+}
+
+export default Phone;
