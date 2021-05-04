dcbc4b4223fad57e7fe1cc24927f41f5e6e7764e
SwarzChen <aaaa102234@gmail.com>
2021-05-05 00:47:23 +0800

    [main] add second page

---
+++ src/pages/second-page/index.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import "./index.css"
+import Billboard from "../../components/billboard/index"
+
+const SecondPage = () => {
+    return (
+        <div className="second-page-container">
+            <Billboard></Billboard>
+        </div>
+    );
+}
+
+export default SecondPage;
