1ef712e7605e80ee32b75424e5d0017afc3e91bf
SwarzChen <aaaa102234@gmail.com>
2021-05-09 13:04:18 +0800

    [main] activate page 3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import FirstPage from "./pages/first-page/index"
 import SecondPage from "./pages/second-page/index"
+import ThirdPage from "./pages/third-page/index"
 
 function App() {
 	return (
 		<div className="App">
 			<FirstPage />
 			<SecondPage />
+			<ThirdPage />
 		</div>
 	);
 }
